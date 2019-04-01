64dac77d261680992ad50b54964d8d3432153828
BalahaMaster <karpiesiuk.marcin@gmail.com>
2019-04-01 07:03:27 +0200

    dupa

---
+++ MiniProject2/Association.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniProject2
+{
+    class Association
+    {
+        public static Dictionary<Type, Association> LegalAssociations { get; set; }
+        public Type StartClassifier { get; set; }
+        public Type EndClassifier { get; set; }
+        public Role Role { get; set; }
+        private Association(Type startClassifier, Role role, Type endClassifier)
+        {
+            StartClassifier = startClassifier;
+            EndClassifier = endClassifier;
+            Role = role
+        }
+
+    }
+}
---
+++ MiniProject2/ObjectPlusPlus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniProject2
+{
+    [Serializable]
+    public enum Role { }
+    public class ObjectPlusPlus : ObjectPlus
+    {
+        public Dictionary<Role, Dictionary<object, ObjectPlusPlus>> Constrains { get; set; }
+        private static HashSet<ObjectPlusPlus> AllParts { get; set; }
+        }
+
+        private void AddConstrain()
+        {
+
+        }
+
+        private void AddPart()
+        {
+
+        }
+    }
+}
