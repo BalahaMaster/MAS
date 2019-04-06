85bc5b3496569e077d4a6250954b58fec8834558
BalahaMaster <karpiesiuk.marcin@gmail.com>
2019-04-06 10:16:29 +0200

    ...

--- MiniProject2/Connection.cs
+++ MiniProject2/Connection.cs
@@ -9,71 +9,14 @@ namespace MiniProject2
     {
         public string Name { get; set; }
         public double Distance { get; set; }
-        public Address StartAddress 
-        { 
-            get
-            {
-                return (Address) GetLinks(Role.ConnectionStartAddress).FirstOrDefault();
-            }
-            set
-            {
-                AddLink(GetAssociation(Role.ConnectionStartAddress), value);
-            }
-        }
-        public Address EndAddress 
-        { 
-            get
-            {
-                return (Address) GetLinks(Role.ConnectionEndAddress).FirstOrDefault();
-            }
-            set
-            {
-                AddLink(GetAssociation(Role.ConnectionEndAddress), value);
-            }
-        }
-        public List<Execution> Executions
-        {
-            get
-            {
-                return (List<Execution>)GetLinks(Role.Connection_Execution).ToList().ConvertAll(x => (Execution)x);
-            }
-            set
-            {
-                foreach(Execution e in GetLinks(Role.Shipment_Execution))
-                {
-                    RemoveLink(GetAssociation(Role.Connection_Execution), e);
-                }
-                foreach(Execution e in value)
-                {
-                    AddLink(GetAssociation(Role.Connection_Execution), e);
-                }
-            }
-        }
-
-        public Schedule Schedule
-        {
-            get
-            {
-                return (Schedule) GetLinks(Role.ConnectionShedule).FirstOrDefault();
-            }
-        }
         public Connection(string name, double distance) : base()
         {
-            Executions = new List<Execution>();
             Name = name;
             Distance = distance;
         }
-
-        public void CreateSchedule()
-        {
-            Schedule.CreateSchedule(this);
-        }
-
         public override string ToString() 
         {
             return Name;
         }
-
-
     }
 }
